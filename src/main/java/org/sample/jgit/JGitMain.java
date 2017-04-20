2b7575c1dcfb5b727a804a97069b568201f8d13c
津崎 <i1658119@cse.kyoto-su.ac.jp>
2017-04-20 10:34:28 +0900

    雛形の作成

---
+++ src/main/java/org/sample/jgit/JGitMain.java
@@ -0,0 +1,103 @@
+package org.sample.jgit;
+
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.StringArrayOptionHandler;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.Status;
+/**
+ * Hello world!
+ *
+ */
+public class JGitMain 
+{
+    private static final String REMOTE_URL = "https://github.com/takehiroman/samplejgit.git";
+
+    @Option(name= "-ini", aliases= "--init", usage= "print git init")
+    private boolean initFlag;
+
+    @Option(name= "-add", aliases= "--add", usage= "print git add")
+    private boolean addFlag;
+
+    @Option(name= "-cmt", aliases= "--commit", usage= "print git commit")
+    private boolean commitFlag;
+
+    @Option(name= "-sts", aliases= "--status", usage= "print git status")
+    private boolean statusFlag;
+
+    @Option(name= "-cln", aliases= "--clone", usage= "print git clone")
+    private boolean cloneFlag;
+
+    public static void main(String[] args)throws IOException, GitAPIException {
+        JGitMain jgit = new JGitMain();
+
+        CmdLineParser parser = new CmdLineParser(jgit);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Repository repository;
+
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        repository = builder.setGitDir(new File("./myrepo/.git")).build();
+        Git git = new Git(repository);
+
+        if(jgit.initFlag){
+            		Git.init()
+		               .setDirectory(new File("myrepo")) /*myrepoというディレクトリ作成*/
+		               .setBare(false)
+		               .call();
+        }
+
+        if(jgit.addFlag){
+                 git.add()
+                    .addFilepattern(".")
+                    .call();
+
+        }
+
+        if(jgit.commitFlag){
+                git.commit()
+                   .setMessage("コミットする")
+                   .call();
+        }
+
+        if(jgit.statusFlag){
+            Status status = git
+                            .status()
+                            .call();
+                System.out.println("Added: " + status.getAdded());
+                System.out.println("Changed: " + status.getChanged());
+                System.out.println("Conflicting: " + status.getConflicting());
+                System.out.println("ConflictingStageState: " + status.getConflictingStageState());
+                System.out.println("IgnoredNotInIndex: " + status.getIgnoredNotInIndex());
+                System.out.println("Missing: " + status.getMissing());
+                System.out.println("Modified: " + status.getModified());
+                System.out.println("Removed: " + status.getRemoved());
+                System.out.println("Untracked  files: " + status.getUntracked());
+                System.out.println("UntrackedFolders: " + status.getUntrackedFolders());
+
+        }
+
+        if(jgit.cloneFlag){
+                 Git result = Git.cloneRepository()
+                                 .setURI(REMOTE_URL)
+                                 .setDirectory(new File("testrepo"))
+                                 .call();
+
+        }
+    }
+}
